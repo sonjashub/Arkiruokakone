123de1583151c3751694ab89a575abffcb53e98a
sonjashub <154900738+sonjashub@users.noreply.github.com>
2024-11-25 20:19:07 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let ruoka = [" wokki", " makaronilaatikko", " salaatti", " burgeri", " kanakastike", " torillat", " tortillavuoka", " lohikeitto", " uunilohi", " lihapullat", " nakkikastike", " uuniperuna", " pizza", " pasta", " lasagne", " bowl", " kalapuikot", " katkarapucurry", " butter chicken", " paistettu riisi", " kasvissosekeitto", " kana-kookoskeitto", " tomaattikeitto", " risotto", " linssikeitto"];
+let ruoka = [" uunimakkara", " wings", " uunifetapasta", " mustapaputacot", " gnocchit", " kananfileepihvit", " liuttusloota", " wokki", " makaronilaatikko", " salaatti", " burgeri", " kanakastike", " torillat", " tortillavuoka", " lohikeitto", " uunilohi", " lihapullat", " nakkikastike", " uuniperuna", " pizza", " pasta", " lasagne", " bowl", " kalapuikot", " katkarapucurry", " butter chicken", " paistettu riisi", " kasvissosekeitto", " kana-kookoskeitto", " tomaattikeitto", " risotto", " linssikeitto"];
 function getRandomElements(arr, numElements) {
     const shuffled = arr.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, numElements);
